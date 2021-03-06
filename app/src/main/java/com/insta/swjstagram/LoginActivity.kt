2b3ef0d365dd8c7534c6e05b3d206b71b41d08d9
wonjun <159632_@naver.com>
2021-03-06 14:31:43 +0900

    Add Google init

--- app/src/main/java/com/insta/swjstagram/LoginActivity.kt
+++ app/src/main/java/com/insta/swjstagram/LoginActivity.kt
@@ -4,14 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.insta.swjstagram.databinding.ActivityLoginBinding
+import java.lang.Exception
 
 class LoginActivity : AppCompatActivity() {
     var auth : FirebaseAuth? = null
+    var googleSingInClient : GoogleSignInClient? = null
+    private var GOOGLE_LOGIN_CODE = 9001
     private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,48 +27,64 @@ class LoginActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         auth = FirebaseAuth.getInstance()
-        binding.apply {
-            emailLoginButton.setOnClickListener {
-                signInAndSignUp()
+        binding.emailLoginButton.setOnClickListener {
+            signInAndSignUp()
+        }
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSingInClient = GoogleSignIn.getClient(this, gso)
+    }
+    private fun googleLogin(){
+        var signInIntent = googleSingInClient?.signInIntent
+s
+        val requestActivity = registerForActivityResult(ActivityResultContracts.StartActivityForResult()
+        ){
+            activityResult ->
+            if(activityResult.resultCode == GOOGLE_LOGIN_CODE){
+
             }
         }
     }
 
     private fun signInAndSignUp(){
-        binding.apply {
+        try {
             auth?.createUserWithEmailAndPassword(
-                emailEdittext.text.toString(),
-                passwordEdittext.text.toString())?.addOnCompleteListener { task ->
-                    if(task.isSuccessful){
+                binding.emailEdittext.text.toString(),
+                binding.passwordEdittext.text.toString())?.addOnCompleteListener { task ->
+                when {
+                    task.isSuccessful -> {
                         // Creating a user account
                         moveMainPage(task.result?.user)
-                    }else if(!task.exception?.message.isNullOrEmpty()){
+                    }
+                    task.exception?.message.isNullOrEmpty() -> {
                         // Show the Error Message
                         Toast.makeText(applicationContext, task.exception?.message, Toast.LENGTH_LONG).show()
                     }
-                    else{
+                    else -> {
                         // Login if  you have account
                         signInEmail()
                     }
+                }
             }
+        }catch (e:Exception){
+            e.printStackTrace()
         }
     }
     private fun signInEmail(){
-        binding.apply {
-            auth?.signInWithEmailAndPassword(
-            emailEdittext.text.toString(),
-                passwordEdittext.text.toString())?.addOnCompleteListener { task ->
-                if(task.isSuccessful){
-                    // Creating a user account
-                    moveMainPage(task.result?.user)
-                }
-                else{
-                    // Show the Error Message
-                    Toast.makeText(applicationContext, task.exception?.message, Toast.LENGTH_LONG).show()
-                }
+        auth?.signInWithEmailAndPassword(
+            binding.emailEdittext.text.toString(),
+            binding.passwordEdittext.text.toString())?.addOnCompleteListener { task ->
+            if(task.isSuccessful){
+                // Creating a user account
+                moveMainPage(task.result?.user)
+            }
+            else{
+                // Show the Error Message
+                Toast.makeText(applicationContext, task.exception?.message, Toast.LENGTH_LONG).show()
             }
         }
-
     }
     private fun moveMainPage(user : FirebaseUser?){
         if(user != null){
